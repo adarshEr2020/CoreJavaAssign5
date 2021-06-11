3a5aa47625f60d0814e66da01e5f5852ded3ff21
adarshEr2020 <adarshprajapati31@gmail.com>
2021-06-11 16:23:51 +0530

    commited all

---
+++ src/com/corejava/QuotientRemainder.java
@@ -0,0 +1,18 @@
+package com.corejava;
+import java.util.Scanner;
+public class QuotientRemainder {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter dividend and divisor : ");
+		Scanner input = new Scanner(System.in);
+		int dividend = input.nextInt();
+		int divisor = input.nextInt();
+		input.close();
+		int quotient = dividend / divisor;
+		int remainder = dividend % divisor;
+		
+		System.out.println("quotient : " +quotient +"/n" +"remainder " +remainder);
+	}
+
+}
